e804fca0c818b21f07e531a7aca52efdabf93cb7
ulfsh <99659380+ulfaashila@users.noreply.github.com>
2022-04-06 11:41:43 +0700

    Add files via upload

---
+++ lat7_PHP/Task.php
@@ -0,0 +1,16 @@
+<?php 
+
+class Task extends DB{
+	
+	// Mengambil data
+	function getTask(){
+		// Query mysql select data ke tb_to_do
+		$query = "SELECT * FROM tb_to_do";
+
+		// Mengeksekusi query
+		return $this->execute($query);
+	}
+	
+}
+
+?>
